e307cf91c45c4685cda180466a7745339e9fe317
NAZIRwill29 <59091291+NAZIRwill29@users.noreply.github.com>
2022-02-08 16:10:32 +0800

    finish

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>JSX Challenge</h1>
+    <p>hey</p>
+  </div>,
+  document.querySelector("#root")
+);
